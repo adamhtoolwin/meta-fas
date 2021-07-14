ffea678336fc846abb6d4b9b20a88d933569a7c0
AdamHtooLwin <adamhtoolwin@gmail.com>
2021-07-14 19:02:27 +0700

    Add FAS metrics

--- meta_oulu.py
+++ meta_oulu.py
@@ -16,6 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from datasets.datasets import Dataset, get_train_augmentations, get_test_augmentations
 from models.scan import SCAN, ResNet18Classifier
+import metrics
 
 import learn2learn as l2l
 
@@ -87,6 +88,10 @@ def main(configs, writer, lr=0.005, maml_lr=0.01, iterations=1000, ways=5, shots
     for iteration in range(iterations):
         iteration_error = 0.0
         iteration_acc = 0.0
+        iteration_acer = 0.0
+        iteration_apcer = 0.0
+        iteration_npcer = 0.0
+
         for _ in range(tps):
             learner = meta_model.clone()
             train_task = train_tasks.sample()
@@ -116,14 +121,26 @@ def main(configs, writer, lr=0.005, maml_lr=0.01, iterations=1000, ways=5, shots
             valid_error = loss_func(predictions, evaluation_labels)
             valid_error /= len(evaluation_data)
             valid_accuracy = accuracy(predictions, evaluation_labels)
+            acer, apcer, npcer = metrics.get_metrics(predictions.argmax(dim=1).cpu().numpy(), evaluation_labels.cpu())
+
             iteration_error += valid_error
             iteration_acc += valid_accuracy
+            iteration_acer += acer
+            iteration_apcer += apcer
+            iteration_npcer += npcer
 
         iteration_error /= tps
         iteration_acc /= tps
+        iteration_acer /= tps
+        iteration_apcer /= tps
+        iteration_npcer /= tps
 
         writer.add_scalar('Loss (iteration)', iteration_error, iteration)
         writer.add_scalar('Accuracy', iteration_acc, iteration)
+
+        writer.add_scalar('Metrics (training)/acer', iteration_acer, iteration)
+        writer.add_scalar('Metrics (training)/apcer', iteration_apcer, iteration)
+        writer.add_scalar('Metrics (training)/npcer', iteration_npcer, iteration)
 
         print('Iteration: {:d} Loss : {:.3f} Acc : {:.3f}'.format(iteration, iteration_error.item(), iteration_acc))
 
